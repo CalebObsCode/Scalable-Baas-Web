2a875ef996cda03120755b91d9c1c490736f9e9d
CalebObsCode <Sommskycal345@gmail.com>
2021-08-11 04:01:14 -0400

    Added README

---
+++ src/main/java/com/example/BAaaS/BAaaSApplication.java
@@ -0,0 +1,13 @@
+package com.example.BAaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BAaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BAaaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BAaaS/BAaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BAaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BAaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
